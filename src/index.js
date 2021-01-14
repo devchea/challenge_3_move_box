900c7345783a964393b751e399d328bb6ec49844
devchea <34663914+devchea@users.noreply.github.com>
2021-01-14 09:52:37 -0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <h1>Move the Box!</h1>
 
       {/* handle the click event on this button */}
+      <button>👆 Move Up 👆</button>
       <button>👆 Move Up 👆</button>
 
       {/* move this box using inline styles */}
@@ -19,5 +20,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
